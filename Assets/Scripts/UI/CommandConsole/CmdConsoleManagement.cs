e24e33e7698e0723be195a2e0ef458dd48687f13
wheat32 <nickpage3232@yahoo.com>
2018-02-06 18:57:16 -0500

    Small command console is working

--- Assets/Scripts/UI/CommandConsole/CmdConsoleManagement.cs
+++ Assets/Scripts/UI/CommandConsole/CmdConsoleManagement.cs
@@ -2,12 +2,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 using UnityEngine;
 
 public class CmdConsoleManagement : MonoBehaviour
 {
     private CmdConsoleSmall cmdConsoleSmall;
     private GameObject GO_cmdConsoleSmall;
+    private CommandProcessor commandProcessor;
 
     public bool consoleUse = true;
 
@@ -22,6 +24,7 @@ public class CmdConsoleManagement : MonoBehaviour
         {
             GO_cmdConsoleSmall = GameObject.Find("CommandConsole/CmdConsole_sml");
             cmdConsoleSmall = GO_cmdConsoleSmall.GetComponent<CmdConsoleSmall>();
+            commandProcessor = this.GetComponent<CommandProcessor>();
             Application.logMessageReceived += HandleLog;
         }
         else
@@ -56,8 +59,27 @@ public class CmdConsoleManagement : MonoBehaviour
         }
         else if (Input.GetKeyDown(Bindings.Console) == true && this.GetComponent<Canvas>().enabled == true)//Close the console
         {
+            if (GO_cmdConsoleSmall.GetComponentInChildren<UnityEngine.UI.InputField>().text.Length > 0)
+            {
+                GO_cmdConsoleSmall.GetComponentInChildren<UnityEngine.UI.InputField>().text = GO_cmdConsoleSmall.GetComponentInChildren<UnityEngine.UI.InputField>().text.Substring(0, GO_cmdConsoleSmall.GetComponentInChildren<UnityEngine.UI.InputField>().text.Length - 1);
+            }
+
             closeConsole();   
         }
+
+        //Get command
+        if (Input.GetKeyDown(KeyCode.Return) == true)
+        {
+            //If small console is up
+            if (GO_cmdConsoleSmall.activeInHierarchy == true && GO_cmdConsoleSmall.GetComponentInChildren<UnityEngine.UI.InputField>().isFocused == true 
+                && GO_cmdConsoleSmall.GetComponentInChildren<UnityEngine.UI.InputField>().text != "")
+            {
+                commandProcessor.processCommand(GO_cmdConsoleSmall.GetComponentInChildren<UnityEngine.UI.InputField>().text.Substring(0, GO_cmdConsoleSmall.GetComponentInChildren<UnityEngine.UI.InputField>().text.Length-1));
+                GO_cmdConsoleSmall.GetComponentInChildren<UnityEngine.UI.InputField>().text = "";
+            }
+        }
+
+        GO_cmdConsoleSmall.GetComponentInChildren<UnityEngine.UI.InputField>().text = Regex.Replace(GO_cmdConsoleSmall.GetComponentInChildren<UnityEngine.UI.InputField>().text, @"\t|\n|\r", "");
     }
 
     public void closeConsole()
@@ -76,8 +98,22 @@ public class CmdConsoleManagement : MonoBehaviour
     
     void HandleLog(string logString, string stackTrace, LogType type)
     {
-        output.Append(logString);
-        stack.Append(stackTrace);
         typeStack.Add(type);
+
+        if (type == LogType.Error || type == LogType.Assert || type == LogType.Exception)
+        {
+            output.Append("\n<color=red>" + logString + "</color>");
+            stack.Append("\n<i><color=red>" + stackTrace + "</color></i>");
+        }
+        else if(type == LogType.Warning)
+        {
+            output.Append("\n<color=yellow>" + logString + "</color>");
+            stack.Append("\n<i><color=yellow>" + stackTrace + "</color></i>");
+        }
+        else
+        {
+            output.Append("\n" + logString);
+            stack.Append("\n<i>" + stackTrace + "</i>");
+        }
     }
 }
---
+++ Assets/Scripts/UI/Profiler/Profiler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Profiler : MonoBehaviour
+{
+    private bool profilerOn = false;
+
+    public void changeProfiler(int mode)
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (profilerOn == false)
+        {
+            return;
+        }
+    }
+}
